754f1bfe5d8a180266ebfc263c473410ab2996ab
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-06-05 19:26:53 +0430

    Add Navbar logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image"
 import Link from "next/link"
+import Logo from "./dojo-logo.png"
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="dojo helpDesk logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo HelpDesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
