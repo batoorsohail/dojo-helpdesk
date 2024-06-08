b39418830539ff4e74ee88c05b7211a7fe68039a
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-06-08 19:18:59 +0430

    Fetch TicketDetails data

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,5 +1,16 @@
-function TicketDetails({params}) {
-  const id = params.id
+async function getTicket(id) {
+  const res = await fetch('http://localhost:4000/tickets/' + id, {
+    next: {
+      revalidate: 60
+    }
+  });
+
+  return res.json();
+}
+
+async function TicketDetails({params}) {
+  const ticket = await getTicket(params.id)
+
   return (
     <main>
       <nav>
