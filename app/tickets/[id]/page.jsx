ac565ab251f24dd9723197ffd02cf9a1f8b7a570
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-06-09 15:39:29 +0430

    Update files

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,3 +1,13 @@
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets');
+
+  const tickets = await res.json;
+
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
+
 async function getTicket(id) {
   const res = await fetch('http://localhost:4000/tickets/' + id, {
     next: {
