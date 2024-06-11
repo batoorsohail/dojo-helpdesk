90cd23a9d474189cee0e8dba3a5d71353cf8bdd7
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-06-11 19:06:19 +0430

    Add handleSubmit function

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -1,18 +1,37 @@
 "use client"
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const CreateForm = () => {
-  // const router = useRouter();
+  const router = useRouter();
 
   const [ title, setTitle ] = useState('');
   const [ body, setBody ] = useState('');
   const [ priority, setPriority ] = useState('low');
   const [ isLoading, setIsLoading ] = useState(false);
 
+  const handleSubmit = async () => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const ticket = {
+      title, body, priority, user_email: 'sohail@netninja.dev',
+    }
+
+    const res = fetch('http://localhost:4000/tickets', {
+      method: "POST",
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify(ticket),
+    })
+
+    if (res.status === 201) {
+      router.push('/ticket')
+    }
+  }
+
   return (
-    <form className="w-1/2">
+    <form onSubmit={handleSubmit} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
