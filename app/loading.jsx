39297caa4d56918d9d3104ce2b67f32b8a877e10
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-06-11 18:16:57 +0430

    Add loading.jsx file

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <main>
+      <h2 className="primary-text">Loading...</h2>
+      <p>Hopefully not for too long :)</p>
+    </main>
+  )
+}
+
+export default Loading
